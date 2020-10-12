c59d2a6600160407d491d2564f61c4ed48d25f65
Miroslava <miroslavaroshchina@gmail.com>
2020-10-12 23:57:29 +0300

    Добавьте файлы проекта.

---
+++ DSA_2020__Class_Work_lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSA_2020__Class_Work_lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
