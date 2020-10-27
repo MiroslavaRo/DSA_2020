acdc577f3452251f1d404997ff2902aeb72577e3
Miroslava <miroslavaroshchina@gmail.com>
2020-10-27 18:02:24 +0200

    homework task 1

---
+++ DSA_Class_Work_lesson2/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DSA_Class_Work_lesson2
+{
+    class Program
+    {
+
+        private static List<int> ListItem = new List<int>();
+
+
+        static void Main(string[] args)
+        {
+            //StudentMarks();
+            // StudentMarks2();
+            //SortedDict();
+           // MenuList();
+            //task02();
+            task03();
+            //task04();
+           
+
+
+            static void StudentMarks()
+            {
+                Dictionary<string, int> studentMarks = new Dictionary<string, int>();
+                studentMarks.Add("Jane", 90);
+                studentMarks.Add("Jojo", 80);
+                studentMarks.Add("Peter", 70);
+                studentMarks.Add("Jay", 30);
+
+                while (true) {
+                    Console.WriteLine("--------------------------------------");
+                    foreach ( var studentMark in studentMarks)
+                    {
+                        Console.WriteLine($"Student {studentMark.Key}, mark = {studentMark.Value} ");
+                    }
+                    Console.WriteLine("--------------------------------------");
+                Console.Write("Student name (type 'exit' to exit): ");
+                string name = Console.ReadLine();
+                    if (name == "exit")
+                    {
+                        break;
+                    }
+                    if (studentMarks.ContainsKey(name))
+                    {
+                        Console.WriteLine($"Student '{name}' score is {studentMarks[name]}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Student '{name}' dosen't exist.");
+                    }
+                }
+
+            }
+            static void StudentMarks2()
+            {
+                Dictionary<string, int> studentMarks = new Dictionary<string, int>();
+                studentMarks.Add("Jane", 90);
+                studentMarks.Add("Jojo", 80);
+                studentMarks.Add("Peter", 70);
+                studentMarks.Add("Jay", 30);
+
+                while (true)
+                {
+                    Console.WriteLine("--------------------------------------");
+                    foreach (var studentMark in studentMarks)
+                    {
+                        Console.WriteLine($"Student {studentMark.Key}, mark = {studentMark.Value} ");
+                    }
+                    Console.WriteLine("--------------------------------------");
+                    Console.Write("Student name (type 'exit' to exit): ");
+                    string name = Console.ReadLine();
+                    if (name == "exit")
+                    {
+                        break;
+                    }
+                    
+                    if (studentMarks.TryGetValue(name, out int value))
+                    {
+                        Console.WriteLine($"Student '{name}' score is {value}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Student '{name}' dosen't exist.");
+                    }
+                }
+
+            }
+             static void SortedDict()
+             {
+
+             }
+             static void MenuList()
+            {
+                while (true)
+                {
+                    Console.WriteLine("1. Add item");
+                    Console.WriteLine("1. Remove item");
+                    Console.WriteLine("3. List of all item");
+                    Console.WriteLine("4. Exit");
+
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            AddItem();
+                            break;
+                        case "2":
+                            RemoveItem();
+                            break;
+                        case "3":
+                            DisplayItem();
+                            break;
+                        case "4":
+                            return;
+
+                    }                   
+                    
+                }
+
+            }
+            static void AddItem()
+            {
+                Console.WriteLine("Item = ");
+                int item = int.Parse(Console.ReadLine());
+                ListItem.Add(item);
+            }
+            static void RemoveItem()
+            {
+                Console.WriteLine("Item to remove = ");
+                int item = int.Parse(Console.ReadLine());
+                ListItem.Remove(item);
+            }
+            static void DisplayItem()
+            {
+                foreach(var item in ListItem)
+                {
+                    Console.WriteLine($"Item = {item}");
+                }
+                Console.WriteLine("Press Enter to countinue...");
+                Console.ReadLine();
+            }
+
+             static void task02()
+            {
+                int [] array = { 3, 4, 4, 2, 3, 3, 4, 3, 2};
+                IDictionary<int, int> numberCount = new SortedDictionary<int, int>();
+                
+                foreach (int num in array)
+                {
+                    int count = 1;
+                    if (numberCount.ContainsKey(num))
+                    {
+                        count = numberCount[num] + 1;
+                    }
+                    numberCount[num] = count;
+                }
+                foreach (var n in numberCount)
+                {
+                    Console.WriteLine($"{n.Key} -> {n.Value} times");
+                }
+
+            }
+             static void task03()
+            {
+                int[] array = { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2, 6, 6, 6 };
+                IDictionary<int, int> numberCount = new SortedDictionary<int, int>();
+
+                foreach (int num in array)
+                {
+                    int count = 1;
+                    if (numberCount.ContainsKey(num))
+                    {
+                        count = numberCount[num] + 1;
+                    }
+                    numberCount[num] = count;
+                }
+                
+                List <int> list= new List<int>();
+                foreach(var w in array)
+                {
+                    if (numberCount[w] % 2 == 0)
+                    {
+                        list.Add(w);
+                    }
+                }
+                Console.Write("List: ");
+                foreach(var w in list)
+                {
+                    Console.Write(w+" ");
+                }
+
+
+            }
+            /* static void task03()
+            {
+
+            }*/
+            /* static void task04()
+            {
+
+            }*/
+
+
+        }
+    }
+}
---
+++ DSA_Homework_2/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace DSA_Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task01();
+           //task02();
+
+
+        }
+         static void task01()
+         {
+            Console.WriteLine("\t-----------Minesweeper Game-----------\n");
+            int n = 0;
+            int m = 0;
+            while (true)
+            {
+                Console.Write("Define the size of the battlefield: ");
+                
+                    if (int.TryParse(Console.ReadLine(), out int x))
+                    {
+                        n = x;
+                        m = x;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not integer vallue can`t be the size.");
+                    }
+                
+            }
+            int[,] battlefield = new int[n,m];
+            int w = 0; 
+            int h = 0;
+            Random ran = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {                                      
+                    battlefield[i, j] = ran.Next(0, 2); 
+                }
+            }
+           
+            Console.WriteLine();
+            Console.WriteLine("\t(Press 'ex' if you want to end a game)\n");
+            bool b = true;
+            while (b) {
+                bool col = false;
+                bool row = false;
+
+                
+                do
+                {
+                    Console.Write("Enter the vertical coordinate:  ");
+                    string str = Console.ReadLine();
+                    if (str != "ex")
+                    {
+                        if (int.TryParse(str, out int y))
+                        {
+                            if (y < n)
+                            {
+                                h = y - 1;
+                                col=true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Vallue which is larger than size can`t be the coordinate. Please, try again.");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not integer vallue can`t be the coordinate. Please, try again.");
+                        }
+                    }
+                    else
+                    {
+                        goto breakOut;
+                    }
+                } while (col==false);
+               
+                
+                do
+                {
+                    Console.Write("Enter the horizontal coordinate: ");
+                    string str = Console.ReadLine();
+                    if (str != "ex")
+                    {
+                        if (int.TryParse(str, out int x))
+                        {
+                            if (x < m)
+                            {
+                                w = x - 1;
+                                row = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Vallue which is larger than size can`t be the coordinate. Please, try again.");
+
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not integer vallue can`t be the coordinate. Please, try again.");
+                        }
+                    }
+                    else
+                    {
+                        goto breakOut;
+                    }
+                }
+                while (row == false);
+                
+                if (battlefield[h, w] == 0)
+                {
+                    Console.WriteLine("YEAH!");
+                }
+                else 
+                {
+                    Console.WriteLine("BOOM!");
+                    goto breakOut;
+                }
+
+            breakOut:
+                Console.WriteLine("\n\t ------End of the game------");
+                Console.WriteLine("\nYour battlefield: \n");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (battlefield[i, j] == 1)
+                        {
+                            Console.Write("X ");
+                        }
+                        else
+                        {
+                            Console.Write("O ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                b = false;
+                
+
+            }   
+
+         }
+      
+        /* static void task02()
+         {
+
+         }*/
+        /* static void task03()
+        {
+
+        }*/
+        /* static void task04()
+        {
+
+        }*/
+        /* static void task05()
+        {
+
+        }*/
+
+    }
+}
